cc95fe8108a68c8f5f2839a2f5bc9b5b0f1390b5
Samuel <samelus1998@outlook.de>
2022-09-02 11:18:16 +0200

    Added APM config

--- src/main/java/de/smart/organizr/SmartOrganizrApplication.java
+++ src/main/java/de/smart/organizr/SmartOrganizrApplication.java
@@ -1,5 +1,6 @@
 package de.smart.organizr;
 
+import co.elastic.apm.attach.ElasticApmAttacher;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SmartOrganizrApplication {
 
 	public static void main(final String[] args) {
+		ElasticApmAttacher.attach();
 		SpringApplication.run(SmartOrganizrApplication.class, args);
 	}
 }
