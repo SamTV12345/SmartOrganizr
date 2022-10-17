53b20657f1c83c8ac5e17d688ef7a22802bbe2b3
Samuel <samuel1998.schwanzer@gmail.com>
2022-10-17 17:37:25 +0200

    Added redirect for ui path.

---
+++ src/main/java/de/smart/organizr/configuration/WebConfiguration.java
@@ -0,0 +1,16 @@
+package de.smart.organizr.configuration;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Controller
+public class WebConfiguration implements WebMvcConfigurer {
+
+	// Match everything without a suffix (so not a static resource)
+	@RequestMapping(value = "/ui/{path:[^.]*}")
+	public String redirect() {
+		// Forward to home page so that route is preserved.(i.e forward:/intex.html)
+		return "forward:/ui/index.html";
+	}
+}
