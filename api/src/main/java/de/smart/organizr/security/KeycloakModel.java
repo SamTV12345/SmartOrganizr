db77504481c3ac155bc9be6498922f8943a645a5
SamTV12345 <40429738+SamTV12345@users.noreply.github.com>
2022-12-02 21:27:06 +0100

    Added @Data for the keycloak model.

--- api/src/main/java/de/smart/organizr/security/KeycloakModel.java
+++ api/src/main/java/de/smart/organizr/security/KeycloakModel.java
@@ -1,11 +1,13 @@
 package de.smart.organizr.security;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
 import org.springframework.hateoas.RepresentationModel;
 
 @AllArgsConstructor
 @Getter
+@Data
 public class KeycloakModel extends RepresentationModel<KeycloakModel> {
 	private final String clientId;
 	private final String url;
